a3106ae4b56b5baec2f1da4f4c07026910164564
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-24 22:41:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let student = {
+  name: 'John Doe'
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  // Create an empty array to hold the keys
+  let keys = [];
+
+  // Loop through all the keys in the object and add them to the array
+  for (let key in this) {
+    if (this.hasOwnProperty(key)) {
+      keys.push(key);
+    }
+  }
+
+  // Return the array of keys
+  return keys;
+};
